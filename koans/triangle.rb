1b2d5eddc3dab9f0042b97d081c9cb3fc19b30e7
Jikku Jose <jikkujose@gmail.com>
2014-05-28 08:19:01 +0530

    170

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,8 +13,36 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  # WRITE THIS CODE
+def triangle(a,b,c)
+  sides = [a,b,c].sort
+
+  if includes_zero?(sides) ||
+     one_side_not_greater_than_the_other_two?(sides) ||
+     has_negative_sides?(sides) 
+    raise TriangleError
+  end
+
+  case sides.uniq.size
+  when 1
+    :equilateral
+  when 2
+    :isosceles
+  when 3
+    :scalene
+  end
+
+end
+
+def includes_zero?(sides)
+  sides.include?(0)
+end
+
+def one_side_not_greater_than_the_other_two?(sides)
+  sides[2] >= sides[1] + sides[0]
+end
+
+def has_negative_sides?(sides)
+  sides.inject(:*) < 0
 end
 
 # Error class used in part 2.  No need to change this code.
